35174ebbc576561663b6015426a252b32b17c8ab
supa-ysk8 <supa.ysk8.19@gmail.com>
2020-01-22 00:08:13 +0900

    implement some modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import _ from 'lodash';
+// import { NAME, NiJou } from './utilities';書き方いろいろ
+// import * as utilities from './utilities';
+// import { NAME as NAME_OF_HAM } from './utilities';
+import Lion from './utilities';
 
 "use strict";
 
 {
+  // console.log(utilities.NiJou(3));
+  // console.log(utilities.NAME);
+  // console.log(NAME_OF_HAM);
+  console.log(Lion.say());
 
   let component = () => {
     const element = document.createElement('div');
